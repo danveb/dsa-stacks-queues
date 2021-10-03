94a38806f4b344e8a694f5e89ece3a028336a45c
Daniel Bae <danibae91@icloud.com>
2021-10-03 11:16:06 -0700

    add methods inside class Stack

--- stack.js
+++ stack.js
@@ -21,25 +21,53 @@ class Stack {
 
   push(val) {
 
+    let newNode = new Node(val) 
+
+    // case: check if stack is empty
+    if(this.size === 0) {
+      this.first = null 
+      this.last = null 
+    }
+
+    if(!this.first) {
+      this.first = newNode
+      this.last = newNode
+    } else {
+      let temporary = this.first;
+      this.first = newNode;
+      this.first.next = temporary;
+    }
+
+    this.size++;
+
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
+    // case: check if stack is empty
+    if(this.size === 0) {
+      throw new Error('Stack is empty') 
+    }
 
+    let temporary = this.first
+    
+    this.first = this.first.next 
+    this.size--
+    return temporary.val 
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    return this.first.val 
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0 
   }
 }
 
