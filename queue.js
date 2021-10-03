cbde627790699fedb64746ac2c1f9f4acc78551a
Daniel Bae <danibae91@icloud.com>
2021-10-02 20:58:49 -0700

    add methods inside class Queue to add value to end and remove value from start

--- queue.js
+++ queue.js
@@ -20,6 +20,19 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    // initiate newNode 
+    let newNode = new Node(val) 
+    // case: check if queue is empty 
+    if(this.size === 0) {
+      // add the node to the queue 
+      this.first = newNode 
+      this.last = newNode
+      this.size++ 
+    } else {
+      this.last.next = newNode 
+      this.last = newNode 
+      this.size++ 
+    }
 
   }
 
@@ -27,7 +40,24 @@ class Queue {
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    // case: check if queue is empty 
+    if(this.size === 0) {
+      throw new Error("Can't Dequeue, Queue is empty...")
+    }
 
+    let data = this.first 
+
+    // case: check if size of the queue is 1 
+    if(this.size === 1) {
+      // first is first 
+      this.first = data 
+      this.last = null 
+
+    } 
+    this.first = this.first.next 
+    this.size-- 
+    return data.val 
+    
   }
 
   /** peek(): return the value of the first node in the queue. */
